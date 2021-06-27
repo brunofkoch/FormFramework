5b98bcf4c1f254c8899e9a5e4a31ace16a94be21
pablo santos <pablojs2008@hotmail.com>
2021-06-27 15:03:59 -0300

    update migrations

---
+++ FormFramework/Migrations/202106251238420_Update_ModProvas.cs
@@ -0,0 +1,18 @@
+﻿namespace FormFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update_ModProvas : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ModProvas", "NumProva", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ModProvas", "NumProva");
+        }
+    }
+}
